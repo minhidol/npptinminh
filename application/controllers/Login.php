10c8ae1e66953c2c6fa336ab40b4f6ebffb3b36b
minhnuyen <minhnn1403@gmail.com>
2022-10-19 21:39:16 +0700

    new

---
+++ application/controllers/Login.php
@@ -0,0 +1,66 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     * 	- or -  
+     * 		http://example.com/index.php/welcome/index
+     * 	- or -
+     * Since this controller is set as the default controller in 
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see http://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index() {
+        $this->load->helper('html');
+        $this->load->helper('url');
+        
+        $this->load->view('login');
+    }
+    public function check_login(){
+        try {
+            $user = $_POST;
+            $this->load->model('user_model','user');
+            $login = $this->user->get_login($user['username']);
+            if(!empty($login) && password_verify($user['password'], $login->password)){
+                $this->session->set_userdata(array('user_id' => $login->id, 'role' => $login->role));
+                echo json_encode(array('status' => 'success'));die;
+            }
+            echo json_encode(array('status' => 'error'));
+          //  echo json_encode(['a' =>]);
+        } catch (Exception $e) {
+            echo json_encode(array('status' => 'errordb'));
+            // this will not catch DB related errors. But it will include them, because this is more general. 
+            //echo json_encode($e);
+            //echo json_encode(array('status' => $e));
+        }
+        // $user = $_POST;
+        // $this->load->model('user_model','user');
+        // $login = $this->user->get_login($user['username']);
+        // return 1;
+        // if(!empty($login) && password_verify($user['password'], $login->password)){
+        //     $this->session->set_userdata(array('user_id' => $login->id, 'role' => $login->role));
+        //     echo json_encode(array('status' => 'success'));die;
+        // }
+        
+        // echo json_encode(array('status' => 'error'));
+    }
+    public function logout(){
+        $this->session->unset_userdata(['user_id', 'role']);
+        $this->load->helper('url');
+        redirect('login');
+    }
+
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeigniter/framework' => array(
+            'pretty_version' => '3.1.13',
+            'version' => '3.1.13.0',
+            'reference' => 'bcb17eb8ba53a85de154439d0ab8ff1bed047bc9',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../codeigniter/framework',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
